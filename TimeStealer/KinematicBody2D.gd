d3074b3a2193c40fe0e01d9ecefe5ebc25a76a36
demonlost <demon.lebedev.00@gmail.com>
2020-04-20 23:24:32 +0500

    added simple movement script

--- TimeStealer/KinematicBody2D.gd
+++ TimeStealer/KinematicBody2D.gd
@@ -1,24 +1,22 @@
 extends KinematicBody2D
 
+const ACCELERATION = 500
+const MAX_SPEED = 150
+const FRICTION = 500
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2.ZERO
+onready var animationPlayer = $AnimationPlayer
 
-
-# Called when the node enters the scene tree for the first time.
-# func _ready():
-#	print("123") # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_down"):
-		move_and_collide(Vector2.DOWN)
-	if Input.is_action_pressed("ui_up"):
-		move_and_collide(Vector2.UP)
-	if Input.is_action_pressed("ui_left"):
-		move_and_collide(Vector2.LEFT)
-	if Input.is_action_pressed("ui_right"):
-		move_and_collide(Vector2.RIGHT)
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
 	
