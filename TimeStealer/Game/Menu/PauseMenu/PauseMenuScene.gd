39a27946bbc43092f62c2874c1e3b58b379dbf4f
w1jtoo <wanadooht@gmail.com>
2020-04-26 21:58:04 +0500

    Background screen added

--- TimeStealer/Game/Menu/PauseMenu/PauseMenuScene.gd
+++ TimeStealer/Game/Menu/PauseMenu/PauseMenuScene.gd
@@ -1,5 +1,6 @@
 extends Control
 
+signal background_fade_finish
 
 var scene_path_to_load
 
@@ -25,5 +26,19 @@ func _on_OptionsButton_pressed():
 	_on_Button_pressed($Buttons/OptionsButton.scene_to_load)
 
 func _on_RestartButton_button_up() -> void:
-	# get_tree().paused = false
+	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+func background_fade_in():
+	$BackgroundFadeIn/AnimationPlayer.play("BackgroundFadeIn")
+	
+func rev_background_fade_in():
+	$BackgroundFadeIn/AnimationPlayer.play("RevFadeIn")
+
+
+func _on_BackgroundFadeIn_background_fade_finish():
+	emit_signal("background_fade_finish")
+
+
+func _on_Back_button_up():
+	rev_background_fade_in()
