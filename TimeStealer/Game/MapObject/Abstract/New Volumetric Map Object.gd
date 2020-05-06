9f970be66f6499536a8800c1946a97b9a8321985
w1jtoo <wanadooht@gmail.com>
2020-05-06 23:54:18 +0500

    WIP: walls
    > added two types of walls
    > fixed base volumetric object

---
+++ TimeStealer/Game/MapObject/Abstract/New Volumetric Map Object.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+onready var tween = $Tween
+
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
+		tween.interpolate_property($Sprite, "modulate",
+		 Color(1, 1, 1, 1),  Color(1, 1, 1, 0.40), 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		tween.start()
+		
+
+
+func _on_Area2D_body_exited(body):
+	if body.get_name() == "Player":
+		tween.interpolate_property($Sprite, "modulate",
+		 Color(1, 1, 1, 0.5),  Color(1, 1, 1, 1), 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		tween.start()
